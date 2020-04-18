f7ae3ce3a3f8ef8396f21a3c90e808c47e70dc54
Alex Kaminski <alexdkaminski@gmail.com>
2020-04-18 12:32:29 +1000

    Add unicafe step 2
    Unicafe step 2

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -20,10 +20,47 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState([])
 
-  const goodClick = () => setGood(good + 1)
-  const neutralClick = () => setNeutral(neutral + 1)
-  const badClick = () => setBad(bad + 1)
+  const goodClick = () => {
+    setAll(all.concat(1))
+    setGood(good + 1)
+  }
+  const neutralClick = () => {
+    setAll(all.concat(0))
+    setNeutral(neutral + 1)
+  }
+
+  const badClick = () => {
+    setAll(all.concat(-1))
+    setBad(bad + 1)
+  }
+
+  const getAverage = () => {
+    let total = 0;
+    if (all.length) {
+      for (let i = 0; i < all.length; i++) {
+        total += all[i];
+      }
+      let avg = total / all.length;
+      return avg
+    }
+  }
+
+  const getPositive = () => {
+    let total = all.length;
+    let positiveTotal = 0;
+    if (all.length) {
+      for (let i = 0; i < all.length; i++) {
+        if (all[i] === 1) {
+          positiveTotal += 1;
+        }
+      }
+      let percentagePositive
+      percentagePositive = positiveTotal / total * 100;
+      return (`${percentagePositive}%`)
+    }
+  }
 
   return (
     <div>
@@ -52,6 +89,18 @@ const App = () => {
       <Statistic
         text='Bad'
         statistic={bad}
+      />
+      <Statistic
+        text='All'
+        statistic={all.length}
+      />
+      <Statistic
+        text='Average'
+        statistic={getAverage()}
+      />
+      <Statistic
+        text='Positive'
+        statistic={getPositive()}
       />
     </div>
   )
