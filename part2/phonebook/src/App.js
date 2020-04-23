dccb9e368c49b24d44f99277d3595baef8f2cd5d
Alex Kaminski <alexdkaminski@gmail.com>
2020-04-23 18:24:07 +1000

    Add phonebook step 11/12

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,13 +2,17 @@ import React, { useState,useEffect } from 'react'
 import Search from './components/Search'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
+import Notification from './components/Notification'
 import personService from './services/person'
+import guidGenerator from './utils/guidGenerator'
 
 const App = () => {
   const [ persons, setPersons ] = useState([])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ search, setSearch ] = useState('')
+  const [ errorMessage, setErrorMessage ] = useState(null)
+  const [ successMessage, setSuccessMessage ] = useState(null)
 
   useEffect(() => {
     personService
@@ -21,33 +25,63 @@ const App = () => {
 
   const addPerson = (e) => {
     e.preventDefault()
+    let guid = guidGenerator()
     const personObj = {
       name: newName,
       number: newNumber,
-      id: persons.length + 1
+      id: guid
     }
     // Check if persons name is already added to db
     if (!persons.some(person => person.name === newName)) {
+      console.log(personObj);
       personService
         // Add person to db
         .createPerson(personObj)
         // Add new person to persons state
         .then(returnedPerson => {
           setPersons(persons.concat(returnedPerson))
+          setSuccessMessage(`Added ${personObj.name}`)
+          setTimeout(() => {
+            setSuccessMessage(null)
+          }, 5000);
           setNewName('')
           setNewNumber('')
         })
         .catch(err => console.log(`an error occured: ${err}`))
     } else {
-      if (window.confirm(`${newName} is already added to phonebook, replace the old number with the new one?`)) {
-        let id = persons.filter(person => person.name === newName)[0].id
-        personService
-          .updatePerson(id, personObj)
-          .then(returnedPerson => {
-            setPersons(persons.map(person => person.id !== id ? person : returnedPerson))
-          })
-          .catch(err => console.log(`an error occured: ${err}`))
-      }
+      let person = persons.find(person => person.name === newName)
+      updatePerson(person)
+    }
+  }
+
+  const updatePerson = person => {
+    let id = person.id
+    // Update person
+    if (window.confirm(`${newName} is already added to phonebook, replace the old number with the new one?`)) {
+      let changedPerson = { ...person, number: newNumber }
+      let name = person.name
+      personService
+        .updatePerson(id, changedPerson)
+        .then(returnedPerson => {
+          setPersons(persons.map(person => person.id !== id ? person : returnedPerson))
+          setSuccessMessage(`Updated ${returnedPerson.name}`)
+          setNewName('')
+          setNewNumber('')
+          setTimeout(() => {
+          setSuccessMessage(null)
+        }, 5000);
+        })
+        .catch(err => {
+          setErrorMessage(`Information of ${name} has already been removed from server`)
+          console.log(err)
+          setPersons(persons.filter(person => person.id !== id))
+          setNewName('')
+          setNewNumber('')
+          setTimeout(() => {
+            setSuccessMessage(null)
+          }, 5000);
+
+        })
     }
   }
 
@@ -64,9 +98,8 @@ const App = () => {
   }
 
   const handleDelete = (e) => {
-    let id = parseInt(e.target.id)
+    let id = e.target.id
     let person = persons.filter(person => person.id === id)[0]
-    console.log(person);
     if (window.confirm(`Are you sure you want to delete ${person.name}?`)) {
       personService
       .deletePerson(id)
@@ -84,6 +117,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification errorMessage={errorMessage} successMessage={successMessage}/>
       <Search handleSearchChange={handleSearchChange} search={search}/>
       <h3>Add a new person</h3>
       <PersonForm addPerson={addPerson} newName={newName} handleNameChange={handleNameChange} newNumber={newNumber} handleNumberChange={handleNumberChange}/>
